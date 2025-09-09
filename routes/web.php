2c55af632a0a5ab0601d7f17ac8b8321be05cd0d
Doris Morales <dmorales55@gmail.com>
2025-09-08 21:12:03 -0500

    Intervention Image v3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::controller(ImageController::class)->group(function () {
+  Route::get('/usuario/avatar', 'fileUpload')->name('image.fileUpload');
+  Route::post('/usuario/avatar', 'storeImage')->name('image.store');
+})->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
