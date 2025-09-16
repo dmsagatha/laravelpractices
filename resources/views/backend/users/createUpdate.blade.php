e9a234d2b2668fafce0eb64f5972fb4f5e1a3c53
Doris Morales <dmorales55@gmail.com>
2025-09-15 20:10:28 -0500

    CRUD Usuarios:  validaciones, Intervention Image y Dropzone

--- resources/views/backend/users/createUpdate.blade.php
+++ resources/views/backend/users/createUpdate.blade.php
@@ -37,6 +37,7 @@
 
   @push('styles')
     <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('css/basic.css') }}">
   @endpush
 
   @push('scripts')
